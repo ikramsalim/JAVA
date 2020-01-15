f737d5a4df26c0262ff1ce3ce38fbfb84cfd55ae
Ikram Salim <ikramomar998@gmail.com>
2020-01-15 11:53:14 -0600

    Multiple catch blocks

--- objectsandapis/ch1/TryCatch/src/com/example/java/Main.java
+++ objectsandapis/ch1/TryCatch/src/com/example/java/Main.java
@@ -12,19 +12,30 @@ public class Main {
         try {
             String welcome = "Welcome!";
             //ArrayIndexOutOfBoundsException
-            char [] chars =welcome.toCharArray();
-            char lastChar = chars[chars.length -1];
+            char[] chars = welcome.toCharArray();
+            char lastChar = chars[chars.length - 1];
             System.out.println("Last Char: " + lastChar);
-            //add -1 above
-            //introduce a null pointer  exception
+            //add -1 above to erase the error
+
+            //introduce a null pointer exception
             String nothing = null;
             System.out.println(nothing.length());
-            
-
-        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+        //distinguish the catches by adding sout for each error
+        catch (NullPointerException e){
             e.printStackTrace();
+            System.out.println("Null exception");
             return;
         }
+        catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("Array index exception");
+            return;
+        }
+        catch(Exception e){
+            System.out.println("Any exception");
+        }
+
         System.out.println("Code ran successfully");
     }
 }

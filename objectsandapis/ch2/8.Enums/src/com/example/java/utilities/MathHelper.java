1ceafa37c8d0a34ae218a8df0c3c116a1cd794d9
Ikram Salim <ikramomar998@gmail.com>
2020-01-24 02:12:43 -0600

    use the enum keyword (instead of class or interface), and separate the constants with a comma. Note that they should be in uppercase letters

---
+++ objectsandapis/ch2/8.Enums/src/com/example/java/utilities/MathHelper.java
@@ -0,0 +1,46 @@
+package com.example.java.utilities;
+import javax.swing.*;
+
+public class MathHelper {
+    //ENUM
+    //enum value can be defined ONCE
+    //stored as internal values
+    //each enum value must be unique
+
+//    public static final int ADD = 1001;
+//    public static final int SUBTRACT = 1002;
+
+    private int total;
+
+    public MathHelper(int total){
+        this.total += total;
+    }
+    //GETTER
+    public int getTotal(){ return total; }
+    // change "int operation" which was constant
+    // to "Operation operation" which identifies enum
+    // in the parameter of the doMath
+    public int doMath(String prompt, Operation operation) throws NumberFormatException{
+        String input = InputHelper.getInput(prompt);
+        int value = Integer.parseInt(input);
+
+        //USE OF CONSTANTS
+        switch (operation){
+            case ADD:
+                total += value;
+                break;
+            case SUBTRACT:
+                total -= value;
+                break;
+
+
+        }
+        total +=value;
+        return value;
+    }
+
+
+
+
+
+}
